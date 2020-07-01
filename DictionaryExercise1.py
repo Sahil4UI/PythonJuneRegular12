df9296c21973a78839c5a200e9eef0b484181775
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-07-01 13:26:25 +0530

    Add files via upload

---
+++ DictionaryExercise1.py
@@ -0,0 +1,5 @@
+X = {"id":[101,102,103,104,105],
+              "Name":["Ram","Krish","Ramesh","Suresh","Kapil"]}
+
+for i in range(len(X["Name"])):
+    print(X["id"][i] ,"---->", X["Name"][i])
---
+++ dictonaryExercise2.py
@@ -0,0 +1,10 @@
+students = [
+    {"id":101,"name":"Ram",marks:[90,56,76]},
+    {"id":102,"name":"Shyam",marks:[80,59,100]}
+    {"id":103,"name":"Sam",marks:[10,15,22]}
+    {"id":104,"name":"krish",marks:[5,50,73]}
+    {"id":105,"name":"Aryan",marks:[89,87,70]}
+    {"id":106,"name":"Rahul",marks:[12,10,8]}
+    {"id":107,"name":"Shiva",marks:[90,100,100]}
+
+    ]
